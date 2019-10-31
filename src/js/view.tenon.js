5943f60f5470b13d8d83ccc7bce564beb5b1ca06
Joe Dolson <design@joedolson.com>
2019-10-31 15:19:30 -0500

    Spaces to tabs

--- src/js/view.tenon.js
+++ src/js/view.tenon.js
@@ -64,17 +64,14 @@
 		}
 	});
 
-
-
-
 }(jQuery));
 
 function xPathToCss(xpath) {
-    return xpath
-        .replace(/\[(\d+?)\]/g, function(s,m1){ return '['+(m1-1)+']'; })
-        .replace(/\/{2}/g, '')
-        .replace(/\/+/g, ' > ')
-        .replace(/@/g, '')
-        .replace(/\[(\d+)\]/g, ':eq($1)')
-        .replace(/^\s+/, '');
+	return xpath
+		.replace(/\[(\d+?)\]/g, function(s,m1){ return '['+(m1-1)+']'; })
+		.replace(/\/{2}/g, '')
+		.replace(/\/+/g, ' > ')
+		.replace(/@/g, '')
+		.replace(/\[(\d+)\]/g, ':eq($1)')
+		.replace(/^\s+/, '');
 }
